222d4b9b4f1860f106868a4f703f678f958b2c15
Geteo <geteolastpiece@gmail.com>
2017-05-11 17:02:51 +0200

    en urgent : creer notification, update du numéro lecon, paramétre au WS

--- OC_Projet3/app/src/main/java/com/openclassrooms/julien/oc_projet3/viewLecons.java
+++ OC_Projet3/app/src/main/java/com/openclassrooms/julien/oc_projet3/viewLecons.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -25,8 +27,13 @@ import java.net.URL;
 
 public class viewLecons extends AppCompatActivity
 {
-    private lecons lecon = null;
-    public TextView textLecon = null;
+    private lecons      lecon       = null;
+    public  TextView    textLecon   = null;
+    public  TextView    textTitreDate;
+    public  TextView    date;
+    public  TextView    textTitreDateSuivante;
+    public  TextView    dateSuivante;
+    private Button      boutonTerminer;
 
     public viewLecons ()
     {
@@ -38,7 +45,14 @@ public class viewLecons extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lecons);
 
-        textLecon = ((TextView) findViewById(R.id.text_lecon));
+        textLecon               = ((TextView) findViewById(R.id.text_lecon));
+        textTitreDate           = ((TextView) findViewById(R.id.textView_titre_date));
+        date                    = ((TextView) findViewById(R.id.textView_date));
+        textTitreDateSuivante   = ((TextView) findViewById(R.id.textView_titre_prochaine_date));
+        dateSuivante            = ((TextView) findViewById(R.id.textView_prochaine_date));
+        boutonTerminer          = ((Button) findViewById(R.id.button_termine_video));
+
+        boutonTerminer.setOnClickListener(terminer);
 
         new lecons().execute("http://192.168.1.37/oc_p3_webservices/webservice.php");
 
@@ -55,6 +69,15 @@ public class viewLecons extends AppCompatActivity
 
         Log.e("Text lecon base : ", textLecon.toString());
     }
+
+    public View.OnClickListener terminer = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            /*update id_lecon de l'utilisateur*/
+        }
+    };
 
     //AsyncTask
 
@@ -123,8 +146,109 @@ public class viewLecons extends AppCompatActivity
                 Log.e("VL", textLecon.toString());
                 JSONArray jArray = new JSONArray(response.toString());
                 textLecon.setText(jArray.getJSONObject(0).getString("nom").toString());
+                date.setText(jArray.getJSONObject(0).getString("date").toString());
+                dateSuivante.setText(jArray.getJSONObject(0).getString("date_suivante").toString());
             }
             catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+
+        }
+
+// Converting InputStream to String
+
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return response.toString();
+        }
+    }
+
+
+    public class UpdateLecon extends AsyncTask<String , Void ,String>
+    {
+        private  String server_response;
+        private  StringBuffer response;
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoInput (true);
+                urlConnection.setDoOutput (true);
+                urlConnection.setUseCaches (false);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+
+                //Create JSONObject here
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("nom", /*MainActivity.CURRENT_USER*/"JulienOC");
+               /* OutputStreamWriter out = new   OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(jsonParam.toString());*/
+
+                // Send POST output.
+                DataOutputStream printout = new DataOutputStream(urlConnection.getOutputStream ());
+                //printout.writeBytes(URLEncoder.encode(jsonParam.toString(),"UTF-8"));
+                printout.writeBytes(jsonParam.toString());
+                printout.flush ();
+                printout.close ();
+
+                int responseCode = urlConnection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    server_response = readStream(urlConnection.getInputStream());
+                    Log.v("CatalogClient", server_response);
+                }
+                else
+                {
+                    Log.e("HTTP_KO !!!", server_response);
+                }
+                //out.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.e("Response", "" + server_response);
+            try
+            {
+
+            }
+            catch (Exception e)
             {
                 e.printStackTrace();
             }
